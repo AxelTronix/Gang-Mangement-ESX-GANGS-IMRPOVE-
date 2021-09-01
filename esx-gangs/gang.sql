70186c1ffbe30fda6e86c7e5ca99f3f09124694c
Bradley <68006496+bradley1337@users.noreply.github.com>
2021-09-01 22:21:42 +0100

    Create gang.sql

---
+++ esx-gangs/gang.sql
@@ -0,0 +1,23 @@
+CREATE TABLE IF NOT EXISTS `gangs` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `label` varchar(255) DEFAULT NULL,
+  `inventory` longtext DEFAULT NULL,
+  `leadership_rank` int(11) NOT NULL DEFAULT 3,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=20 DEFAULT CHARSET=latin1;
+
+
+CREATE TABLE IF NOT EXISTS `gang_ranks` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `label` varchar(255) DEFAULT NULL,
+  `gang_name` varchar(255) DEFAULT NULL,
+  `ranking` int(11) DEFAULT NULL,
+  `vehicles` longtext DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=92 DEFAULT CHARSET=latin1;
+
+
+
+ALTER TABLE users ADD `gang` varchar(50) COLLATE utf8mb4_bin DEFAULT NULL, ADD `gang_rank` int(11) DEFAULT NULL;
